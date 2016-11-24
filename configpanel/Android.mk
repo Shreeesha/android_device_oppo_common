97f2b97e4f73207392953a5d36c32d27e91c7afc
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-11-23 18:13:45 -0800

    oppo-common: configpanel: Commonize strings translations
     * Translations moved to the new project
        android_packages_resources_devicesettings
    
    Change-Id: I06536915e1963a2fe464c3d6c46dd8728aba0cb4

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -21,6 +21,13 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res
+
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_PACKAGE)
